ea6fd78a30f5e130679a1b2b99f9cd956f475e65
Peter Collingbourne <peter@pcc.me.uk>
2010-10-10 04:36:26 +0100

    VpnServices: supply authentication method parameter to racoon
    This patch causes VpnServices to supply the new authentication
    method parameter when invoking racoon.
    
    Change-Id: I3eaf3ffe0276de6f191fab3021dcaf97bdde5b2b

--- packages/VpnServices/src/com/android/server/vpn/VpnDaemons.java
+++ packages/VpnServices/src/com/android/server/vpn/VpnDaemons.java
@@ -60,14 +60,14 @@ class VpnDaemons implements Serializable {
     public DaemonProxy startIpsecForL2tp(String serverIp, String pskKey)
             throws IOException {
         DaemonProxy ipsec = startDaemon(IPSEC);
-        ipsec.sendCommand(serverIp, L2TP_PORT, pskKey);
+        ipsec.sendCommand("psk", serverIp, L2TP_PORT, pskKey);
         return ipsec;
     }
 
     public DaemonProxy startIpsecForL2tp(String serverIp, String userKeyKey,
             String userCertKey, String caCertKey) throws IOException {
         DaemonProxy ipsec = startDaemon(IPSEC);
-        ipsec.sendCommand(serverIp, L2TP_PORT, userKeyKey, userCertKey,
+        ipsec.sendCommand("rsa", serverIp, L2TP_PORT, userKeyKey, userCertKey,
                 caCertKey);
         return ipsec;
     }
